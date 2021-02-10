3cda1e3228cadccc9b0b183214c16c0e0806e04c
Diego <consultoresartificialbrain@gmail.com>
2021-02-10 10:55:18 -0600

    agregando todo al repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Helmet } from 'react-helmet';
 import './scss/styleEspinoza.scss';
 import { MenuProvider } from './context/carritoContext';
 
+
 export default function App() {
 	return (
 		<div>
