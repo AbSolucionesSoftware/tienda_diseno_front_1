357fe7fc4525a0956cb65b4bf9a66645d9a479d1
poday98 <poday736.98@gmail.com>
2021-02-12 16:42:33 -0600

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'antd/dist/antd.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from './config/routes';
 import { Helmet } from 'react-helmet';
-import './scss/styleEspinoza.scss';
+import './scss/disenoUno.scss';
 import { MenuProvider } from './context/carritoContext';
 
 
